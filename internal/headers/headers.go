a2df4c6b8cdaf9cb9a04b49a510c2792aa9ff32c
horribleinternet <cyberblah@gmail.com>
2025-10-01 21:37:37 +0900

    finish adding the header parsing

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -24,25 +24,59 @@ func NewHeaders() Headers {
 
 func (h Headers) Parse(data []byte) (n int, done bool, err error) {
 	index := bytes.Index(data, lineEndBytes)
-	fmt.Printf("index: %d, %s\n", index, string(data[:index]))
+	//fmt.Printf("index: %d, %s\n", index, string(data[:index]))
 	if index < 0 {
 		return 0, false, nil
 	} else if index == 0 {
 		return lineEndLen, true, nil
 	} else {
 		key, value, found := strings.Cut(string(data[:index]), ":")
-		fmt.Println(key, value)
+		//fmt.Println(key, value)
 		if !found {
-			return 0, false, fmt.Errorf("no key:value pair found")
+			return 0, false, fmt.Errorf("no field-name:field-value pair found")
 		}
 		keyTrim := strings.TrimRightFunc(key, func(r rune) bool { return unicode.IsSpace(r) })
 		if keyTrim != key {
-			return 0, false, fmt.Errorf("whitespace after key")
+			return 0, false, fmt.Errorf("whitespace after field name '%s'", key)
 		}
 		key = strings.TrimLeftFunc(key, func(r rune) bool { return unicode.IsSpace(r) })
+		if !validFieldName(key) {
+			return 0, false, fmt.Errorf("invalid character in field name '%s'", key)
+		}
+		key = strings.ToLower(key)
 		value = strings.TrimFunc(value, func(r rune) bool { return unicode.IsSpace(r) })
-		fmt.Println(key, value)
-		h[key] = value
+		existing, there := h[key]
+		if there {
+			h[key] = existing + ", " + value
+		} else {
+			h[key] = value
+		}
 		return index + lineEndLen, false, nil
+	}
+}
+
+func validFieldName(fieldName string) bool {
+	if len(fieldName) == 0 {
+		return false
+	}
+	for _, r := range fieldName {
+		if unicode.IsLetter(r) || unicode.IsDigit(r) {
+			continue
+		}
+		if _, valid := validRuneSet[r]; !valid {
+			return false
+		}
+	}
+	return true
+}
+
+const validRunes = "!#$%&'*+-.^_`|~"
+
+var validRuneSet map[rune]struct{}
+
+func init() {
+	validRuneSet = make(map[rune]struct{})
+	for _, r := range validRunes {
+		validRuneSet[r] = struct{}{}
 	}
 }
--- internal/headers/headers_test.go
+++ internal/headers/headers_test.go
@@ -15,7 +15,7 @@ func TestHeadersParse(t *testing.T) {
 	n, done, err := headers.Parse(data)
 	require.NoError(t, err)
 	require.NotNil(t, headers)
-	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, "localhost:42069", headers["host"])
 	assert.Equal(t, 23, n)
 	assert.False(t, done)
 
@@ -32,8 +32,8 @@ func TestHeadersParse(t *testing.T) {
 	total += n
 	require.NoError(t, err)
 	require.NotNil(t, headers)
-	assert.Equal(t, "localhost:42069", headers["Host"])
-	assert.Equal(t, "freakonaleash69", headers["gUest"])
+	assert.Equal(t, "localhost:42069", headers["host"])
+	assert.Equal(t, "freakonaleash69", headers["guest"])
 	assert.Equal(t, 26, n)
 	assert.False(t, done)
 	n, done, err = headers.Parse(data[total:])
@@ -49,16 +49,32 @@ func TestHeadersParse(t *testing.T) {
 	assert.Equal(t, 0, n)
 	assert.False(t, done)
 
+	// Test: Invalid field name
+	headers = NewHeaders()
+	data = []byte("Hos t: localhost:42069       \r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.Error(t, err)
+	assert.Equal(t, 0, n)
+	assert.False(t, done)
+
+	// Test: Different invalid field name
+	headers = NewHeaders()
+	data = []byte("H,ost: localhost:42069       \r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.Error(t, err)
+	assert.Equal(t, 0, n)
+	assert.False(t, done)
+
 	// Test: Valid double header with existing headers with done
 	headers = NewHeaders()
 	headers["billy"] = "bob"
 	headers["frank"] = "joke"
-	data = []byte(" Host: localhost:42069  \r\n  gUest: freakonaleash69\r\n\r\n")
+	data = []byte(" Billy: Briggs  \r\n  gUest: freakonaleash69\r\n\r\n")
 	n, done, err = headers.Parse(data)
 	total = n
 	require.NoError(t, err)
 	require.NotNil(t, headers)
-	assert.Equal(t, 26, n)
+	assert.Equal(t, 18, n)
 	assert.False(t, done)
 	n, done, err = headers.Parse(data[total:])
 	total += n
@@ -66,10 +82,9 @@ func TestHeadersParse(t *testing.T) {
 	require.NotNil(t, headers)
 	assert.Equal(t, 26, n)
 	assert.False(t, done)
-	assert.Equal(t, "bob", headers["billy"])
+	assert.Equal(t, "bob, Briggs", headers["billy"])
 	assert.Equal(t, "joke", headers["frank"])
-	assert.Equal(t, "localhost:42069", headers["Host"])
-	assert.Equal(t, "freakonaleash69", headers["gUest"])
+	assert.Equal(t, "freakonaleash69", headers["guest"])
 	n, done, err = headers.Parse(data[total:])
 	assert.Equal(t, 2, n)
 	assert.True(t, done)
