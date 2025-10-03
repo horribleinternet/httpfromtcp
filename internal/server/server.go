0aa7f8ec56e77a9806239f0668b24f4affa4e229
horribleinternet <cyberblah@gmail.com>
2025-10-04 00:20:49 +0900

    package to handle incoming http connectons

---
+++ internal/server/server.go
@@ -0,0 +1,50 @@
+package server
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"sync/atomic"
+)
+
+type Server struct {
+	listener net.Listener
+}
+
+var closed atomic.Bool
+
+func Serve(port int) (*Server, error) {
+	portStr := fmt.Sprintf(":%d", port)
+	listener, err := net.Listen("tcp", portStr)
+	if err != nil {
+		return nil, err
+	}
+	closed.Store(false)
+	server := Server{listener: listener}
+	go server.listen()
+	return &server, nil
+}
+
+func (s *Server) Close() error {
+	closed.Store(true)
+	return s.listener.Close()
+}
+
+func (s *Server) listen() {
+	for {
+		conn, err := s.listener.Accept()
+		if err != nil {
+			if closed.Load() {
+				break
+			}
+			log.Printf("error accepting connection: %v", err)
+			continue
+		}
+		go s.handle(conn)
+	}
+}
+
+func (s *Server) handle(conn net.Conn) {
+	conn.Write([]byte("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nHello World!\n"))
+	conn.Close()
+}
